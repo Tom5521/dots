3a93fd162ddbaf4c92d73d2ce2c334114b7b4ef9
Tom5521 <thomas.bored5521@gmail.com>
2025-07-18 01:52:17 -0300

    Improve screenshot script!

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env bash
 
-main() {
-  path="$(xdg-user-dir PICTURES)/Screenshots/\
-  $(date +"%d-%m-%Y %H:%M:%S").png"
+must_toggle_hyprshade() {
+  command -v hyprshade &>/dev/null
+  local has_hyprshade=$?
+  local is_hyprshade_on
 
-  hyprshot -m region --raw >"$path"
-  sleep 1 # Wait til' the data is moved to the file.
+  if [ $has_hyprshade -eq 0 ]; then
+    [[ "$(hyprshade current)" != "" ]]
+    is_hyprshade_on=$?
+  fi
+
+  [[ $has_hyprshade -eq 0 && $is_hyprshade_on -eq 0 ]]
+  return $?
+}
+must_toggle_hyprshade
+must_toggle_hyprshade=$?
+
+main() {
+  local screenshot_path
+  screenshot_path="$(xdg-user-dir PICTURES)/Screenshots/\
+    $(date +"%d-%m-%Y %H:%M:%S").png"
+
+  local current_shader
+  if [ $must_toggle_hyprshade -eq 0 ]; then
+    current_shader=$(hyprshade current)
+    hyprshade off
+  fi
+
+  hyprshot -m region --raw >"$screenshot_path"
+  sleep 1.5 # Wait til' the data is moved to the file.
+
+  if [ $must_toggle_hyprshade -eq 0 ]; then
+    hyprshade on "$current_shader"
+  fi
 
   # Operation cancelled.
-  if [[ "$(stat -c "%s" "$path")" == "0" ]]; then
-    rm "$path"
+  if [[ "$(stat -c "%s" "$screenshot_path")" == "0" ]]; then
+    rm "$screenshot_path"
     return
   fi
-  copyq write image/png - <"$path"
+
+  copyq write image/png - <"$screenshot_path"
 }
 
 main
