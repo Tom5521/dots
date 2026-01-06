86b8ba708f283d633ae13d382bb9464c255555ae
Tom5521 <thomas.bored5521@gmail.com>
2026-01-05 21:04:15 -0300

    update dots

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,32 +1,10 @@
 #!/usr/bin/env bash
 
-must_toggle_hyprshade() {
-  command -v hyprshade &>/dev/null
-  local has_hyprshade=$?
-  local is_hyprshade_on
-
-  if [ $has_hyprshade -eq 0 ]; then
-    [[ "$(hyprshade current)" != "" ]]
-    is_hyprshade_on=$?
-  fi
-
-  [[ $has_hyprshade -eq 0 && $is_hyprshade_on -eq 0 ]]
-  return $?
-}
-must_toggle_hyprshade
-must_toggle_hyprshade=$?
-
 main() {
   local screenshot_path
   screenshot_path="$(xdg-user-dir PICTURES)/Screenshots/$(date +"%d-%m-%Y %H:%M:%S").png"
 
   mkdir -p "$(dirname "$screenshot_path")"
-
-  local current_shader
-  if [ $must_toggle_hyprshade -eq 0 ]; then
-    current_shader=$(hyprshade current)
-    hyprshade off
-  fi
 
   hyprshot -m region --freeze -s --raw >"$screenshot_path"
 
@@ -34,10 +12,6 @@ main() {
   while lsof "$screenshot_path" >/dev/null 2>&1; do
     sleep 0.1s
   done
-
-  if [ $must_toggle_hyprshade -eq 0 ]; then
-    hyprshade on "$current_shader"
-  fi
 
   copyq write image/png - <"$screenshot_path"
   copyq select 0
